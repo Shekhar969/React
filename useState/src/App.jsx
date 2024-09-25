b0c6b670d62c9628a8dc762036bc1c3bcc7baae5
Shekhar969 <shekharrawal96@gmail.com>
2024-09-25 19:46:06 +0545

    UseState

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
+
+    return(
+        <>
+        <button>-</button>
+        <span>0</span>
+        <button>+</button>
+        </>
+    )
+
+
 }
 
 export default App
