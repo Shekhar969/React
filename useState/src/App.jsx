056a625ce1cd9f70a88e9f58ac01a582a1eb17bc
Shekhar969 <shekharrawal96@gmail.com>
2024-09-25 20:22:22 +0545

    UseState Done

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,18 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// liked ? 'liked' : 'did not like'
+// liked ? 'if' : 'else'
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-
-    return(
-        <>
-        <button>-</button>
-        <span>0</span>
-        <button>+</button>
-        </>
-    )
+  const [num, numState] = useState(10);
+  const [name, nameState] = useState("dev");
+  const [age, ageState] = useState(18);
 
 
+
+  function decrementBtn() {
+    // numState(num-1)
+    numState((prevnum) => prevnum - 1);
+  }
+
+  function incrementBtn() {
+    numState(num + 1);
+  }
+
+  function changeName(e){
+        nameState(e.target.value)
+  }
+
+  function incrementAge(){
+    ageState(pervAge => pervAge +1)
+  }
+  return (
+    <>
+      <button onClick={decrementBtn}>-</button>
+      <span> {num} </span>
+      <button onClick={incrementBtn}>+</button>
+
+      <br />
+
+        <input 
+        type="text"
+        value={name}
+        onChange={changeName}
+         />
+
+        <button onClick={incrementAge}>IncrementAge</button>
+
+        <br />
+        <span>Hello {name} Your are Curently {age} year's Old</span>
+
+
+    </>
+  );
 }
 
-export default App
+export default App;
