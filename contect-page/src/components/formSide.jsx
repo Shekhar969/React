47be38023adb7806d7f68e5f06c46cfdf5f09a52
Shekhar969 <shekharrawal96@gmail.com>
2024-09-27 19:31:24 +0545

    formPage Added

--- contect-page/src/components/formSide.jsx
+++ contect-page/src/components/formSide.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Style from "./main.module.css";
+import { useState } from "react";
 
 function FormSide() {
+  const [name,nameState]=useState()
+
+  const btnClicked=()=>{
+
+  }
+  
+  const nameChange=(e)=>{
+    nameState(e.target.value)
+  }
   return (
-    <div>contect form</div>
-  )
+    <div className={Style.formCointener}>
+      <h1>Lets talk</h1>
+      <input type="text" value={name} placeholder="Your Full Name" onChange={nameChange} />
+      <input type="text"  placeholder=" Your Email" />
+      <textarea placeholder="Your Message" />
+      <button onClick={btnClicked}>Submit</button>
+    </div>
+  );
 }
 
-export default FormSide
+export default FormSide;
